bfe0aac50b3dd2175e13cd7daa119482265c9c57
mcseagate <mcseagate@gmail.com>
2022-05-26 11:33:09 -0500

    input_tutorial

---
+++ tutorial.py
@@ -0,0 +1,7 @@
+print('User Details')
+fname = input("Enter fname:")
+lname = input("Enter lname: ")
+dob = input("Enter date of birth: ")
+addr = input("Enter address: ")
+email = input("Enter email: ")
+sex = input("Enter sex: ")
